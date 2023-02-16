02d15fa55096caaa47d6461e26db7edd8e429249
estherodiaka <estherodiaka20@gmail.com>
2023-02-16 14:28:32 -0800

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - main function
+ * main - this is the main function
  *
- * Return: 0 when successful
+ * Return: always 0
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
-} 
+}
