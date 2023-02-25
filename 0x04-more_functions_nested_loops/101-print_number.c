d52607f94fb3f3dc193281db7eb5397c5eaf5482
estherodiaka <estherodiaka20@gmail.com>
2023-02-24 16:02:28 -0800

    task12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,7 +13,7 @@ void print_number(int n)
 	if (n < 0)
 	{
 		n1 = -n;
-		putchar('_');
+		_putchar('_');
 	}
 	else
 	{
@@ -23,5 +23,5 @@ void print_number(int n)
 	{
 		print_number(n1 / 10);
 	}
-	putchar((n1 % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
